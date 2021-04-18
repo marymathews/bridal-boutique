e8c3caefc659a1fb75e74b535f77f03ef9acedb8
marymathews <mathews.mary@gmail.com>
2021-04-18 17:52:18 -0500

    Feature signup (#3)
    * started backend sign up handling
    
    * sign-up in progress
    
    * completed jquery regex for password and email validation
    
    * updated basic UI
    
    * fixed footer not sticking to bottom
    
    * completed sign up form UI
    
    * added redirection to home screen on logo click
    
    * more footer fixes
    
    * added a favicon
    
    * completed password validation
    
    * completed ajax call to check if email is already used
    
    * completed showing error message for existing user
    
    * added error page for unexpected flask errors

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request, redirect, json
+from flaskext.mysql import MySQL
+from flask_bcrypt import Bcrypt
+
+#instantiate flask and bcrypt
+app = Flask(__name__)
+bcrypt = Bcrypt()
+
+#instantiate and setup MySQL
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'boutique'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 8889
+mysql.init_app(app)
+
+#set up secret key (discuss?)
+app.secret_key = 'Secret Key'
+
+#set up a route for the default page (root URL)
+@app.route("/")
+def main():
+	return render_template('index.html')
+
+#route to show signup page
+@app.route("/showSignUp")
+def showSignUp():
+	return render_template("signUp.html")
+
+#route to sign up new user
+@app.route("/signUp", methods = ['POST'])
+def signUp():
+
+	try:
+		#read the form data
+		_email = request.form['email']
+		_name = request.form['name']
+		_pwd = request.form['password']
+
+		#connect to the db
+		cxn = mysql.connect()
+		cursor = cxn.cursor()
+
+		#hash the password
+		hashed_pwd = bcrypt.generate_password_hash(_pwd)
+
+		#insert into the db only if fields are not empty
+		if(_email and _name and hashed_pwd):
+			cursor.execute("INSERT INTO account(email, name, h_password) VALUES (%s, %s, %s)", (_email, _name, hashed_pwd))
+			data = cursor.fetchall()
+			if(len(data) == 0):
+				cxn.commit()
+				return redirect("/")
+			else:
+				return render_template("error.html", error = 'Something Went Wrong!')
+		else:
+			return render_template("error.html", error = 'A required field is missing!')
+
+	#show the error page if anything goes wrong	
+	except Exception:
+		return render_template("error.html", error = 'Error Signing Up!')
+
+	#close the cursor and db connection
+	finally:
+		cursor.close()
+		cxn.close()
+#todo - return proper response or redirect to correct page
+
+@app.route("/checkExistingEmail/<string:email>", methods = ['GET'])
+def checkExistingEmail(email):
+	#connect to db
+	cxn = mysql.connect()
+	cursor = cxn.cursor()
+
+	#check if email is already in db
+	existingUser = False
+	cursor.execute("SELECT * FROM account WHERE email = %s", email)
+	if(len(cursor.fetchall()) > 0):
+		existingUser = True
+
+	cursor.close()
+	cxn.close()
+
+	if(existingUser):
+		return json.dumps({'error': 'Existing Account'})
+	else:
+		return ('', 204)
+
+#make sure the right script is being run
+if __name__ == "__main__":
+	#runs the application from the app variable
+	app.run()
---
+++ static/js/signUp.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    $("#submit").click(function(event) {
+        if(!isEmailValid($("#email").val())) {
+            event.preventDefault();
+            $("#email-validation").css({
+                'color': 'red'
+            });
+        }
+        if(!isPasswordValid($("#password").val())) {
+            event.preventDefault();
+            $("#pwd-validation").css({
+                'color': 'red'
+            });
+        }
+        if(isEmailUsed($("#email").val())) {
+            event.preventDefault();
+            $("#existing-email").show().css({
+                'color': 'red'
+            });
+        }
+    });
+
+    $("#email").focus(function() {
+        $("#email-validation").show();
+        $("#existing-email").hide()
+    });
+
+    $("#email").blur(function() {
+        $("#email-validation").hide();
+    });
+
+    $("#password").focus(function() {
+        $("#pwd-validation").show();
+    });
+
+    $("#password").blur(function() {
+        $("#pwd-validation").hide();
+    });
+})
+
+function isEmailValid(email) {
+    var emailRegex = new RegExp('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$');
+    if(emailRegex.test(email))
+        return true;
+    return false;
+}
+
+function isPasswordValid(pwd) {
+    //minimum 6, maximum 30; at least one uppercase, at least one lowercase, at least one digit
+    var pwdRegex = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,30}/;
+    if(pwdRegex.test(pwd))
+        return true;
+    return false;
+}
+
+function isEmailUsed(value) {
+    var result = false;
+    $.get({
+        url: '/checkExistingEmail/' + value,
+        type: 'GET',
+        dataType: 'json',
+        async: false,
+        success: function(response) {
+            $.each(response, function(key, value) {
+                if(key == 'error' && value == 'Existing Account') {
+                    result = true;
+                }
+            });
+        }
+    });
+    return result;
+}
