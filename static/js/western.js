21ff04c57459ff59d2224e0cd89c1455f4fd3338
mary_m <mathews.mary@gmail.com>
2021-04-23 14:35:43 -0500

    completed pagination

--- static/js/western.js
+++ static/js/western.js
@@ -1,17 +1,22 @@
-// $(document).ready(function() {
-//     fetchProducts(1);
-// })
+$(document).ready(function() {
+    var path = window.location.pathname
+    console.log(path);
 
-// function fetchProducts(page) {
-//     $.ajax({
-//         url: '/getProducts/west/' + page,
-//         type: 'GET',
-//         dataType: 'json',
-//         success: function(response) {
-//             console.log(response);
-//         },
-//         error: function(error) {
-//             console.log(error);
-//         } 
-//     });
-// }
+    if(path.includes(1)) {
+        $("#page-1").addClass("active");
+        $("#page-2").removeClass("active");
+        $("#page-3").removeClass("active");
+    };
+
+    if(path.includes(2)) {
+        $("#page-1").removeClass("active");
+        $("#page-2").addClass("active");
+        $("#page-3").removeClass("active");
+    };
+
+    if(path.includes(3)) {
+        $("#page-1").removeClass("active");
+        $("#page-2").removeClass("active");
+        $("#page-3").addClass("active");
+    };
+});
