60e523301526a12943083124a4863ed331b4d226
mary_m <mathews.mary@gmail.com>
2021-04-23 21:34:06 -0500

    completed sign up, sign in, logout redirection, visiblity, and functionality

--- static/js/itemListing.js
+++ static/js/itemListing.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
             $.each(response, function(key, value) {
                 if(value == 'Logged In') {
                     $("#logout").show();
+                    $("#profile").attr('href', '#');
+                }
+                else {
+                    $("#logout").hide();
+                    $("#profile").attr('href', '/showSignIn');
                 }
             })
         }
