d8b8ad5855a90908e56a6db1d34da76a50f42afd
mary_m <mathews.mary@gmail.com>
2021-04-27 18:59:35 -0500

    temp changes

---
+++ static/js/wishlist.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $(".flexslider").flexslider();
+
+    var isLoggedIn = false;
+    $.get({
+        url: '/checkSignedIn',
+        dataType: 'JSON',
+        success: function(response) {
+            $.each(response, function(key, value) {
+                if(value == 'Logged In') {
+                    isLoggedIn = true;
+                    $("#logout").show();
+                    $("#profile").attr('href', '/showUserProfile');
+                    $("#wishlist").attr('href', '#');
+                    $("#show-wishlist").attr('href', '/showWishlist')
+                }
+                else {
+                    isLoggedIn = false;
+                    $("#logout").hide();
+                    $("#profile").attr('href', '/showSignIn');
+                    $("#wishlist").attr('href', '/showSignIn');
+                    $("#show-wishlist").attr('href', '/showSignIn')
+                }
+            })
+        }
+    });
+
+    $("#wishlist").click(function() {
+        if(isLoggedIn) {
+            $.ajax({
+                url: '/addToWishlist',
+                type: 'PUT',
+                dataType: 'json',
+                data: {item_id: $(this).parent().attr('id')},
+                success: function(response) {
+                    $.each(response, function(key, value) {
+                        if(key == 'error' && value == 'Item not added') {
+                            Swal.fire("Something went wrong!", "", "error");                
+                        }
+                        else {
+                            Swal.fire("Item Added to Wishlist!", "", "success");
+                        }
+                    });
+                },
+                error: function(error) {
+                    Swal.fire("This item is already in your wishlist!", "", "error");                
+                }
+            });
+        }
+    });
+})
