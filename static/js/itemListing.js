b58ce34e6f27848ecefc2b70353be71d4470d3bf
mary_m <mathews.mary@gmail.com>
2021-05-08 22:31:57 -0500

    added indicator for current page

--- static/js/itemListing.js
+++ static/js/itemListing.js
@@ -25,23 +25,14 @@ $(document).ready(function() {
     
     var path = window.location.pathname
 
-    if(path.includes(1)) {
-        $("#page-1").addClass("active");
-        $("#page-2").removeClass("active");
-        $("#page-3").removeClass("active");
-    };
-
-    if(path.includes(2)) {
-        $("#page-1").removeClass("active");
-        $("#page-2").addClass("active");
-        $("#page-3").removeClass("active");
-    };
-
-    if(path.includes(3)) {
-        $("#page-1").removeClass("active");
-        $("#page-2").removeClass("active");
-        $("#page-3").addClass("active");
-    };
+    for(let i = 1; i <= 4; i++) {
+        if(path.includes(i)) {
+            $(`#${i}`).addClass("active");
+        }
+        else {
+            $(`#${i}`).removeClass("active");
+        }
+    }
 
     $(".wishlist").click(function() {
         if(isLoggedIn) {
