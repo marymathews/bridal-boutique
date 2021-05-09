360634a0431321928c8f91c599496f33bd660371
mary_m <mathews.mary@gmail.com>
2021-05-09 09:54:17 -0500

    minor bug fixes

--- static/js/addSizeQty.js
+++ static/js/addSizeQty.js
@@ -5,11 +5,10 @@ $(document).ready(function() {
 		var size = $("#add-item-dropdown").val();
 		var quantity = $("#add-item-quantity").val();
 		$("#add-items-list").append('<input type="text" name=size'+size+' id="'+size+'" class="size-info size-new" value="Size:'+ size + ' Quantity:'+quantity+'" readonly/>');
-		$("option[value='"+size+"']").remove();
+		$("#add-item-dropdown option[value='"+size+"']").remove();
 	});
 
-	$("ul").on("click", "input", function(){
-		console.log(this.id);
+	$("ul").on("click", "input", function() {
 		var size_id = '#'+this.id;
 		$(size_id).remove();
 
@@ -30,7 +29,6 @@ $(document).ready(function() {
 
 	$('.new-entry').on('click', function(e) {
 		e.preventDefault();
-		console.log("clicked new");
 		var id = 0;
 		var newinput = function() {
   			var field = document.createElement("input");
