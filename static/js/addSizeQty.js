f31213ac471524c9d269f8c88abe3d89da955d67
Sai Manasa <vedantamsaimanasa@gmail.com>
2021-05-09 08:21:09 -0500

    Bug fixes

--- static/js/addSizeQty.js
+++ static/js/addSizeQty.js
@@ -1,22 +1,32 @@
 $(document).ready(function() {
+	$(".click-msg").hide();
 	$('.add-entry').on('click',function(e){
 		e.preventDefault();
 		var size = $("#add-item-dropdown").val();
 		var quantity = $("#add-item-quantity").val();
-		$("#add-items-list").append('<input type="text" class="size-info" name=size'+size+' id="'+size+'" class="form-control size-new" value="Size:'+ size + ' Quantity:'+quantity+'" readonly/>');
+		$("#add-items-list").append('<input type="text" name=size'+size+' id="'+size+'" class="size-info size-new" value="Size:'+ size + ' Quantity:'+quantity+'" readonly/>');
 		$("option[value='"+size+"']").remove();
 	});
 
 	$("ul").on("click", "input", function(){
-			console.log(this.id);
-			var size_id = '#'+this.id;
-            $(size_id).remove();
-            $('#add-item-dropdown')
-         		.append($("<option></option>")
-         		.attr("value",this.id)
-         		.text(this.id));
+		console.log(this.id);
+		var size_id = '#'+this.id;
+		$(size_id).remove();
+
+		if ( $('ul').find('input').length === 0) 
+			$(".click-msg").hide();
+        $('#add-item-dropdown')
+         	.append($("<option></option>")
+         	.attr("value",this.id)
+         	.text(this.id));
     });
 
+    $("#add-items-list").on("mouseover", function() {
+		$(".click-msg").show();
+    });
+	$("#add-items-list").on("mouseout", function() {
+		$(".click-msg").hide();
+    });
 
 	$('.new-entry').on('click', function(e) {
 		e.preventDefault();
